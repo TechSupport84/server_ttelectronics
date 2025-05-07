142fd2efd5e6b89693896d02e17db5d6e1addf18
jeancy001 <jeancympoy24@gmail.com>
2025-05-07 14:22:56 -0400

    added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app  = express()
 const port   = process.env.PORT || 3001
 
 app.use(express.json())
-app.use(cors({origin:['https://ttelectronics.netlify.app/'],credentials: true}))
+app.use(cors({origin:['https://ttelectronics.netlify.app'],credentials: true}))
 app.use(cookieParser())
 
 //routers
